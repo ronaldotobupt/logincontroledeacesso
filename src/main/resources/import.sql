ef2f1e389e6a833fa5cc6e117190de9e78ba14c0
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-03 12:33:35 -0300

    BCrypt password encoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$Zi7NIvbf1WbSkBdg.3rwlO3iUbTMimn8B/9tf2PpHz8wymVmRrsrO');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Zi7NIvbf1WbSkBdg.3rwlO3iUbTMimn8B/9tf2PpHz8wymVmRrsrO');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
